6a7f1fc59a9aaf53285e555cdf207aeef4c991b3
arshiaesll <aeslami@login001.cm.cluster>
2025-09-16 12:32:39 -0400

    moving scripts

---
+++ HPC_scripts/gpu_run.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+#SBATCH --job-name=hybridModels
+#SBATCH -N 1    ## requests on 1 node
+#SBATCH -n 28   ## requests on 1 CPU
+#SBATCH --gres=gpu:1 ## requests on 1 GPU
+#SBATCH --output /work/aeslami/VLA_results/job%j.out
+#SBATCH --error /work/aeslami/VLA_results/job%j.err
+#SBATCH -p gpu-v100-32gb
+
+## Limit threads to be equal to slurm request
+# export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
+
+# Load CUDA module, 10.1 worked ok but the quantization gave an error
+module load cuda/12.1
+
+# Load Anaconda module (adjust the path if necessary)
+module load python3/anaconda/2023.9
+
+
+# Activate your conda environment, ## user source activate on cluster, not conda activate
+source activate /work/aeslami/VLA/VLA_env
+
+# Add this line to pass the no_quant flag if it's set to true
+python /work/aeslami/LM_finetune/finetune3/main.py
+
+
