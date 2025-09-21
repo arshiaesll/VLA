ba2e4cb2e149f5a3099aeb0ea44ecc18e3ee21e2
arshiaesll <aeslami@login001.cm.cluster>
2025-09-21 13:05:03 -0400

    updates to inference, train and mathdial dataset processing

--- HPC_scripts/finetune.sh
+++ HPC_scripts/finetune.sh
@@ -1,13 +1,12 @@
 #!/bin/sh
 #SBATCH --job-name=VLMFinetune
 #SBATCH -N 1    ## requests on 1 node
-#SBATCH --gres=gpu:4   # request 2 GPUs
+#SBATCH --gres=gpu:1   # request 2 GPUs
 #SBATCH --time=12:00:00
 #SBATCH --output /work/aeslami/VLA_results/job%j.out
 #SBATCH --error /work/aeslami/VLA_results/job%j.err
-#SBATCH -p gpu-A100
+#SBATCH -p gpu-H200
 
-## Limit threads to be equal to slurm request
 
 # Load CUDA module
 module load cuda12.4
@@ -19,7 +18,6 @@ module load python3/anaconda/3.12
 # Activate your conda environment, ## user source activate on cluster, not conda activate
 source activate /work/aeslami/VLA/VLA_env
 
-# Add this line to pass the no_quant flag if it's set to true
-accelerate launch --multi-gpu /work/aeslami/VLA/main.py train --train-dataset mathdial
-
+# Add this line to pass all the arguments down to main.py
+accelerate launch /work/aeslami/VLA/main.py "$@"
 
